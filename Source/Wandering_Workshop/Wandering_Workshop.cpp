0ec77f7291aa21ac3008af0cbb2c5658fb112e8f
Viktor Mendonck <viktor.mendonck@gmail.com>
2024-12-01 23:15:26 +0100

    init commit

---
+++ Source/Wandering_Workshop/Wandering_Workshop.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Wandering_Workshop.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Wandering_Workshop, "Wandering_Workshop" );
